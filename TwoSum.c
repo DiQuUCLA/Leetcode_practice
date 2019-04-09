df484e8baa54eb9e4d6bdf776c8aad01448e0293
DiQu <dqu2@g.ucla.edu>
2019-04-08 22:23:16 -0700

    P1

---
+++ TwoSum.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    int i, j;
+    for (i = 0; i < numsSize; i++)
+    {
+        for (j = i + 1; j < numsSize; j++)
+        {
+            if(target - nums[i] == nums[j])
+            {
+                result[0] = i;
+                result[1] = j;
+                
+            }
+        }
+    }
+    return result;
+}
